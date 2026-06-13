005c811f02561f089f54c15e7212e7c1253bb80d
mioony <guzybartosz@gmail.com>
2026-06-14 01:05:54 +0200

    Version 1.0: Solved logic issues and finished debugging

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ private:
     int szerokosc;
     int wysokosc;
     int ilosc_min;
+    bool koniec_gry;
     vector<vector<Pole>> siatka;
 
     void rozstawMiny()
@@ -37,32 +38,95 @@ private:
         }
     }
 
+    void obliczLiczby() 
+    {
+        for(int y = 0; y < wysokosc; y++) 
+        {
+            for(int x = 0; x < szerokosc; x++) 
+            {
+                if(siatka[y][x].mina == true) continue;
+                
+                short licznik_min = 0;
+
+                for(int przesuniecie_y = -1; przesuniecie_y <= 1; przesuniecie_y++) {
+                    for(int przesuniecie_x = -1; przesuniecie_x <= 1; przesuniecie_x++) {
+                        
+                        int sprawdz_y = y + przesuniecie_y;
+                        int sprawdz_x = x + przesuniecie_x;
+
+                        if(sprawdz_x >= 0 && sprawdz_x < szerokosc && sprawdz_y >= 0 && sprawdz_y < wysokosc)
+                            if(siatka[sprawdz_y][sprawdz_x].mina == true)
+                                licznik_min++;
+                    }
+                }
+                siatka[y][x].liczba = licznik_min;
+            }
+        }
+    }
+
 public:
     Plansza(int temp_szerokosc, int temp_wysokosc, int temp_ilosc_min)
     {
+        koniec_gry = false;
         szerokosc = temp_szerokosc;
         wysokosc = temp_wysokosc;
         ilosc_min = temp_ilosc_min;
         siatka.resize(wysokosc, vector<Pole>(szerokosc));
         rozstawMiny();
+        obliczLiczby();
     }
 
-private:
-    void obliczLiczby() {
-    for(int y = 0; y < wysokosc; y++) {
-        for(int x = 0; x < szerokosc; x++) {
-            
+    void prawyKlik(int x, int y)
+    {
+        if(siatka[y][x].odkryte == false && siatka[y][x].flaga == false)
+        {
+            siatka[y][x].flaga = true;
+        }
+        else if(siatka[y][x].odkryte == false && siatka[y][x].flaga == true)
+        {
+            siatka[y][x].flaga = false;
         }
     }
-}
+
+    void lewyKlik(int x, int y)
+    {
+        if(siatka[y][x].odkryte == false && siatka[y][x].flaga == false)
+        {
+            siatka[y][x].odkryte = true;
+            if(siatka[y][x].mina == true)
+            {
+                koniec_gry = true;
+            }
+        }
+
+    }
+
+    void debugWyswietl()
+    {
+        for(int i = 0; i < wysokosc; i++)
+        {
+            for(int j = 0; j < szerokosc; j++)
+            {
+                if(siatka[i][j].mina == true)
+                {
+                    cout << "*" ;
+                }
+                else
+                {
+                    cout << siatka[i][j].liczba;
+                }
+            }
+            cout << endl;
+        }
+    }
 
 };
 
 
 
 int main() {
-
-    std::cout << "Test logiki Sapera" << std::endl;
     
+    Plansza moja_gra(10,10,15);
+    moja_gra.debugWyswietl();
     return 0;
 }
