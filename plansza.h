3c7bbcf844d4b57441c700632ce6ae1b8f8dba83
mioony <guzybartosz@gmail.com>
2026-06-14 22:45:36 +0200

    Release: final backend with complete game logic and file refactoring

---
+++ plansza.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+class Plansza 
+{
+public:
+    struct Pole
+    {
+        bool mina;
+        bool flaga;
+        bool odkryte;
+        short liczba;
+    };
+    
+private:
+    int szerokosc;
+    int wysokosc;
+    int ilosc_min;
+    bool koniec_gry;
+    int ile_pol_zostalo;
+    vector<vector<Pole>> siatka;
+
+    void rozstawMiny();
+
+    void obliczLiczby();
+
+public:
+    Plansza(int temp_szerokosc, int temp_wysokosc, int temp_ilosc_min);
+
+    void prawyKlik(int x, int y);
+
+    void lewyKlik(int x, int y);
+
+    void odkryjPola(int x, int y);
+
+    void debugWyswietl();
+
+    void wyswietlPlansze();
+
+    bool czyKoniecGry();
+
+};
